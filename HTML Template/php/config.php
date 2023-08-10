5737063b30d18011257a5a3bf3ef37111212c68a
Pablo91218 <guerre.pablo.agustin@gmail.com>
2023-08-10 19:09:05 -0300

    envio de correo corregir bug

--- HTML Template/php/config.php
+++ HTML Template/php/config.php
@@ -10,20 +10,19 @@
 This data will only be displayed in your inbox when an email arrives.
 ----------------------------------------------*/
 
-$MessageTitle   = 'Correo de consultas';   // Ingrese un título para el correo electrónico que llegará a su bandeja de entrada.
-$RecipientEmail = 'loquevenga2019@gmail.com'; // Enter the your email address that will receive form messages.
-$RecipientName  = 'Pablo Agustin Guerreño';             // Enter your name.
+$MessageTitle   = 'Nuevo mensaje de contacto XD';   // Enter a title for the email that will arrive in your inbox.
+$RecipientEmail = 'guerre.pablo.agustin@gmail.com'; // Enter the your email address that will receive form messages.
+$RecipientName  = 'Pablo Agustin Gerreño';             // Enter your name.
 $ReplyToEmail   = 'no-reply@email.com';    // Enter the email address to which the reply message is sent.
 
 /*----------------------------------------------
 Change this only if you change the name of these fields in the HTML form.
 ----------------------------------------------*/
 
-$FieldSubject = 'subject'; // Subject field name attribute.
-$FieldEmail   = 'email';   // Email field name attribute.
+$FieldSubject = 'contact-name'; // Subject field name attribute.
+$FieldEmail   = 'contact-email';   // Email field name attribute.
 
 /*----------------------------------------------
 Global variables, don't change anything here.
 ----------------------------------------------*/
 global $RecipientEmail, $RecipientName, $ReplyToEmail, $FieldSubject, $FieldEmail;
-?>
--- HTML Template/php/sender.php
+++ HTML Template/php/sender.php
@@ -14,41 +14,59 @@ require 'PHPMailer/src/Exception.php';
 require 'PHPMailer/src/PHPMailer.php';
 require 'PHPMailer/src/SMTP.php';
 
-//Create an instance; passing `true` enables exceptions
-$mail = new PHPMailer(true);
+if ( ! empty( $_POST ) ) {
 
-try {
-    //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-    $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';                     	//Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'loquevenga2019@gmail.com'; 			// Tu dirección de Gmail
-    $mail->Password   = 'Incade30000'; 							// Tu contraseña de Gmail
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+	$mail = new PHPMailer();
+	
+	/*----------------------------------------------
+	Here you can set a Gmail or other email to send via SMTP. 
+	Read more: https://github.com/PHPMailer/PHPMailer
+    $mail->SMTPDebug = 2;*/
+	$mail->isSMTP();
+	$mail->Host       = 'smtp.gmail.com';
+	$mail->SMTPAuth   = true;
+	$mail->SMTPSecure = 'tls';
+	$mail->Username   = 'guerre.pablo.agustin@gmail.com';
+	$mail->Password   = 'ufrhmjbhbmcvxpjh';
+	$mail->Port       = 587;
 
-    //Recipients
-    $mail->setFrom('from@example.com', 'Mailer');
-    $mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient
-    $mail->addAddress('ellen@example.com');               //Name is optional
-    $mail->addReplyTo('info@example.com', 'Information');
-    $mail->addCC('cc@example.com');
-    $mail->addBCC('bcc@example.com');
+	if ( isset( $_POST[$FieldSubject] ) ) {
+		$mail->Subject = $_POST[$FieldSubject];
+	}
 
-    //Attachments
-    $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-    $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+	if ( isset( $_POST[$FieldEmail] ) ) {
+		$mail->setFrom( $_POST[$FieldEmail] );
+	}
 
-    //Content
-    $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = 'Here is the subject';
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+	$mail->addReplyTo( $ReplyToEmail );
+	$mail->addAddress( $RecipientEmail, $RecipientName );
+	$mail->isHTML( true );
 
-    $mail->send();
-    echo 'Message has been sent';
-} catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+	$mail->Body = '<h3>' . $MessageTitle . '</h3>';
+ 
+	// Takes the fields and adds them to the body of the email.
+	foreach( $_POST as $key => $value ) {
+		$mail->Body .= '<p><b>' . str_replace( '-', ' ', ucfirst( $key ) ) . '</b>: ' .  $value . '</p>';
+	}
+
+	// Checks if the email was sent
+	if ( ! $mail->send() ) {
+		$response = array(
+			'status' => 'error',
+			'debug'  => $mail->ErrorInfo
+		);
+
+	} else {
+		echo "<script>alert('Mensaje enviado con éxito');</script>";
+		
+        header("Location: pagina_de_redireccion.php");
+        
+        $response = array(
+			'status' => 'success'
+        );
+    }
+
+	print_r( json_encode( $response ) );
 }
-?>
+
+exit;
